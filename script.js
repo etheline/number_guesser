a458f8d34982f7df9d75e1142a0c7a8f0715fbbb
Etheline Nguyen <38763065+etheline@users.noreply.github.com>
2021-01-30 19:13:40 -0800

    Files for number guessing game

---
+++ script.js
@@ -0,0 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+/* Called at the start of each round to generate the new secret target numberhe new secret target numberhe new secret target number */
+function generateTarget() {
+
+}
+// he new secret target numberhe new secret target numberhe new secret target numberhe new secret target numberhe new secret target numberdfdaf
